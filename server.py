8ba4a8cfbbf989aa8b28a5966e4b3d66be910b65
Janaki Ranpura <automatic.janaki@gmail.com>
2021-02-01 00:33:18 -0500

    complete Skills 3 files

--- server.py
+++ server.py
@@ -41,7 +41,68 @@ MOST_LOVED_MELONS = {
 }
 
 
-# REPLACE THIS WITH YOUR ROUTES
+# MY ROUTES START HERE
+@app.route('/')
+def start_here():
+    """Return homepage."""
+    
+### MUCH EXPLANATION HERE ###
+    # this get request is for a session labeled 'username'
+    # it calls up input from the html that has been assigned to 'userinput' 
+    # this code makes {name: userinput} available to all functions
+    # think of 'session' as data-in-this-session
+    
+    # And saying it another way: 
+    # the userinput called up is stored in a session called 'username'
+    # session is at whatever_name_I_write, and
+    # the get is for what I listed as 'name' = (get the thing) in the referenced html file 
+    # session["username"] = request.args.get("user_input")
+
+    # writing this explicitly for clarity; 
+    # normal syntax is 
+    # session[username] = request.args.get(')
+    # and adding a line here to regularize the
+    # syntax of output by capitalizing
+### END EXPLANATION HERE ###
+
+    user = session.get('username', None)
+    if user != None:   
+        return redirect ('/top-melons')
+    return render_template('homepage.html')
+
+@app.route('/get-name')
+def get_name():
+    """Get the user's name and send to top-melons."""
+    
+    # if userinput in session:
+    #     return render_template
+    
+    input_from_user = request.args.get('userinput')  
+    session['username'] = input_from_user
+    
+    return redirect('/top-melons')                   
+
+
+@app.route('/top-melons')
+def show_top_melons():
+    """Display the most-loved melons."""
+    top_Melons = MOST_LOVED_MELONS
+
+    # if username is a key in the collection "session", 
+    # show topmelons.html
+    # else 
+    # redirect to HomePage
+
+    user = session.get('username', None)
+    if user != None:
+ #   if 'username' in session and session['username'] != None:
+
+        return render_template('top-melons.html',                   
+                                #userinput=session['username'], 
+                                top_melons=MOST_LOVED_MELONS)
+        
+    else:
+         return redirect('/') # go to the beginning
 
 
 if __name__ == '__main__':
